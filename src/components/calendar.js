cdd91d8cff5f3bafaefc7ffbd5a3119a6e2b890b
Lee Naeun <nanni4287@gmail.com>
2022-07-31 12:55:47 +0900

    calendar axios 요청 완료

--- src/components/calendar.js
+++ src/components/calendar.js
@@ -12,7 +12,11 @@ import { customAxios } from '../axios/custromAxios';
 
 function CalendarComp({ setReservationInfo, reservationInfo, queryIdx }) {
   const [calendarMonth, setCalendarMonth] = useState(9);
-  const [activeColor, setActiveColor] = useState({ idx: null, active: false });
+  const [activeColor, setActiveColor] = useState({
+    idx: null,
+    active: false,
+  });
+  const [dateArr, setDateArr] = useState([]);
   const date = ['일', '월', '화', '수', '목', '금', '토'];
   const lastDate = new Date(2022, calendarMonth, 0).getDate();
   const startDay = new Date(2022, calendarMonth - 1).getDay();
@@ -20,12 +24,29 @@ function CalendarComp({ setReservationInfo, reservationInfo, queryIdx }) {
 
   let arrDate = [];
 
+  const getDate = (month) => {
+    if (calendarMonth < 10) {
+      customAxios
+        .get(`reservation/view/${queryIdx}/20220${month}`)
+        .then((r) => {
+          console.log('9', r);
+          setDateArr([...r.data.dateList]);
+        });
+      // setDateArr([{ itemIdx: 1, itemDate: '2022.9.30', isType: 0 }]);
+    } else {
+      customAxios.get(`reservation/view/${queryIdx}/2022${month}`).then((r) => {
+        console.log('10', r);
+        setDateArr([...r.data.dateList]);
+      });
+    }
+  };
+
   useEffect(() => {
-    customAxios.get(`reservation/view/${queryIdx}`).then((r) => console.log(r));
     if (currentDate.getMonth() > calendarMonth - 1) {
       setCalendarMonth(10);
     }
-  }, []);
+    getDate(calendarMonth);
+  }, [calendarMonth]);
 
   const goMonth = (prevOrNext) => {
     if (prevOrNext === 'next') {
@@ -38,32 +59,68 @@ function CalendarComp({ setReservationInfo, reservationInfo, queryIdx }) {
 
   const handleCalendarBody = () => {
     for (let j = 0; j < startDay; j++) {
-      arrDate.push({ date: null, enable: false });
-    }
-    for (let i = 1; i <= lastDate; i++) {
-      if (calendarMonth > currentDate.getMonth()) {
-        if (calendarMonth === 9 && i !== 30) {
-          arrDate.push({ date: i, enable: false });
-        } else if (calendarMonth === 10) {
-          if (currentDate.getMonth() === 9 && currentDate.getDate() > i) {
-            arrDate.push({ date: i, enable: false });
-          } else if (i < 6) {
-            arrDate.push({ date: i, enable: true });
-          } else {
-            arrDate.push({ date: i, enable: false });
-          }
-        } else {
-          arrDate.push({ date: i, enable: true });
-        }
-      } else {
-        arrDate.push({ date: i, enable: false });
-      }
+      arrDate.push({ date: null, itemIdx: 1, itemIdx: null });
     }
 
+    for (let i = 1; i <= lastDate; i++) {
+      const activeDate = dateArr.map((el) => Number(el.itemDate.split('.')[2]));
+      const activeMonth = dateArr.map((el) =>
+        Number(el.itemDate.split('.')[1])
+      );
+
+      if (activeMonth[0] === calendarMonth) {
+        if (activeDate.includes(i)) {
+          arrDate.push({ date: i, itemIdx: 0 });
+        } else {
+          arrDate.push({ date: i, itemIdx: 1 });
+        }
+      } else {
+        arrDate.push({ date: i, itemIdx: 1 });
+      }
+    }
     return arrDate;
   };
 
+  // const handleCalendarBody = () => {
+  //   for (let j = 0; j < startDay; j++) {
+  //     arrDate.push({ date: null, enable: false });
+  //   }
+
+  //   for (let i = 1; i <= lastDate; i++) {
+  //     if (calendarMonth === 9 && i !== 30) {
+  //       arrDate.push({ date: i, enable: false });
+  //     } else if (calendarMonth === 10 && i > 5) {
+  //       arrDate.push({ date: i, enable: false });
+  //     } else {
+  //       arrDate.push({ date: i, enable: true });
+  //     }
+  //   }
+
+  //   // for (let i = 1; i <= lastDate; i++) {
+  //   //   if (calendarMonth > currentDate.getMonth()) {
+  //   //     if (calendarMonth === 9 && i !== 30) {
+  //   //       arrDate.push({ date: i, enable: false });
+  //   //     } else if (calendarMonth === 10) {
+  //   //       if (currentDate.getMonth() === 9 && currentDate.getDate() > i) {
+  //   //         arrDate.push({ date: i, enable: false });
+  //   //       } else if (i < 6) {
+  //   //         arrDate.push({ date: i, enable: true });
+  //   //       } else {
+  //   //         arrDate.push({ date: i, enable: false });
+  //   //       }
+  //   //     } else {
+  //   //       arrDate.push({ date: i, enable: true });
+  //   //     }
+  //   //   } else {
+  //   //     arrDate.push({ date: i, enable: false });
+  //   //   }
+  //   // }
+
+  //   // return arrDate;
+  // };
+
   const handleChangeDate = (idx, date) => {
+    console.log(date, calendarMonth);
     setActiveColor({ idx: idx, active: true });
     setReservationInfo({
       ...reservationInfo,
@@ -107,13 +164,15 @@ function CalendarComp({ setReservationInfo, reservationInfo, queryIdx }) {
           {arrDate.map((el, idx) => (
             <button
               key={idx}
-              style={!el.enable ? { color: '#C5C5C5' } : { color: 'black' }}
+              style={
+                el.itemIdx === 1 ? { color: '#C5C5C5' } : { color: 'black' }
+              }
               onClick={() => handleChangeDate(idx, el.date)}
-              disabled={!el.enable}
+              disabled={el.itemIdx === 1}
             >
               <span
                 style={
-                  activeColor.idx === idx && el.enable
+                  activeColor.idx === idx && el.itemIdx === 0
                     ? { backgroundColor: 'black', color: 'white' }
                     : { backgroundColor: 'transparent' }
                 }
